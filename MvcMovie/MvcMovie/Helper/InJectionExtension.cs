90cc3fc42c49dac46333261f55384a1c85a87344
joe <as955133@gmail.com>
2021-01-18 21:46:04 +0800

    調整注入

--- MvcMovie/MvcMovie/Helper/InJectionExtension.cs
+++ MvcMovie/MvcMovie/Helper/InJectionExtension.cs
@@ -2,10 +2,6 @@
 using Model.Models;
 using Model.Repository;
 using Service.Service;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
 
 namespace MvcMovie.Helper
 {
@@ -13,29 +9,12 @@ namespace MvcMovie.Helper
     {
         public static void InJectionByRepository(this IServiceCollection services)
         {
-            List<string> dataName = new List<string>() { "Repository" };
-            Type[]  types = (from t in Assembly.Load("Model").GetExportedTypes()
-                             where t.IsInterface
-                             select t).ToArray(); 
-            foreach (var item in types)
-            {
-                if (dataName.Contains(item.Name))
-                {
-
-                }
-            }
-            //Object obj = Activator.CreateInstance("Model", "Repository").Unwrap();
-            //MethodInfo[] methods = obj.GetType().GetMethods(BindingFlags.Instance);
- 
-
-            //services.AddScoped<IRepository<Movie>, MovieRepository>();
-
             services.AddScoped<IRepository<Movie>, MovieRepository>();
         }
 
-    public static void InJectionByService(this IServiceCollection services)
-    {
-        services.AddScoped<IMoviesService, MoviesService>();
+        public static void InJectionByService(this IServiceCollection services)
+        {
+            services.AddScoped<IMoviesService, MoviesService>();
+        }
     }
-}
 }
